e09b53f631d9574834de8afcb8062d6b66a485ac
Antoine Gaillard <agaillard@palo-it.com>
2020-08-21 15:50:07 +0200

    first commit with fresh strapi install

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '2037df301550f917904484fcaddf2d65'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '463bdece-54cd-4d58-b2f6-83f2b11ed2fc'
+};
