d1645088866f38ce54bc69c1bd8b219939604695
Antoine Gaillard <agaillard@palo-it.com>
2020-08-26 16:35:30 +0200

    server config variabilised

--- config/server.js
+++ config/server.js
@@ -1,6 +1,6 @@
 module.exports = ({ env }) => ({
   host: env('HOST', '0.0.0.0'),
-  port: env.int('PORT', 1337),
+  port: env.int('PORT', ''),
   admin: {
     auth: {
       secret: env('ADMIN_JWT_SECRET', '2037df301550f917904484fcaddf2d65'),
