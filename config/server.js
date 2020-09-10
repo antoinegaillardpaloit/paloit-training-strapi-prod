b41251e03bf432052464bf0a6053d6a92eabbb1a
Antoine Gaillard <agaillard@palo-it.com>
2020-09-10 13:11:27 +0200

    added url option in server config of strapi

--- config/server.js
+++ config/server.js
@@ -1,6 +1,7 @@
 module.exports = ({ env }) => ({
   host: env('HOST', '0.0.0.0'),
   port: env.int('PORT', ''),
+  url: env('URL', ''),
   admin: {
     auth: {
       secret: env('ADMIN_JWT_SECRET', '2037df301550f917904484fcaddf2d65'),
