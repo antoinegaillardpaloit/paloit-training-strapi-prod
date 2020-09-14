275abaeec98f22771130e8415dc6b57c5b65fedf
Antoine Gaillard <agaillard@palo-it.com>
2020-09-14 10:52:05 +0200

    new field in formation model for slug system

--- api/formation/models/formation.js
+++ api/formation/models/formation.js
@@ -5,4 +5,22 @@
  * to customize this model
  */
 
-module.exports = {};
+const slugify = require('slugify');
+
+module.exports = {
+  /**
+   * Triggered before user creation.
+   */
+  lifecycles: {
+    async beforeCreate(data) {
+      if (data.intitule) {
+        data.slug = slugify(data.intitule, {lower: true});
+      }
+    },
+    async beforeUpdate(params, data) {
+      if (data.intitule) {
+        data.slug = slugify(data.intitule, {lower: true});
+      }
+    },
+  },
+};
